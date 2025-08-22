7338713d7ec753c92c50613d0e4e036d874bb460
shreejoy <sarkarshreejoy30@gmail.com>
2025-08-22 20:23:00 +0530

    changes

--- app.py
+++ app.py
@@ -49,4 +49,4 @@ def predict():
         return render_template('home.html',results=results[0])
     
 if __name__== "__main__":
-    app.run(host = "0.0.0.0",debug=True, port = 5000)
+    app.run(host = "0.0.0.0",debug=True)
